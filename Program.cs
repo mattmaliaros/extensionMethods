8d4b58103640f0f175bd1dee61d6c6b5eb1a6bec
Matt <mattmaliaros@gmail.com>
2019-11-20 16:18:21 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//Linq -- Language Integrated Query--- Capability to query objects
+
+namespace Extension_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed a very long blog post blah blah blah...";
+            var shortened = post.Shorten(5);
+            IEnumerable<int> numbers = new List<int>() {1,3,4,9,2,12,18};
+            var largest = numbers.Max();
+            Console.WriteLine(shortened); //shortens to 5 words
+            Console.WriteLine(largest); // returns largest number in list
+        }
+    }
+}
---
+++ StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+
+// We can declare namespace System since we are using namespace System in Program
+// This is best for Extension methods
+
+
+// ReSharper disable once CheckNamespace
+namespace System
+{
+    public static class StringExtensions
+    {
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            if (numberOfWords < 0)
+                throw new ArgumentOutOfRangeException("numberOfWords of should be greater than or equal to 0");
+            if (numberOfWords == 0)
+                return "";
+          
+
+            var words = str.Split(' ');
+
+            if (words.Length <= numberOfWords)
+                return str;
+            return string.Join(" ", words.Take(numberOfWords));
+        }
+    }
+}
